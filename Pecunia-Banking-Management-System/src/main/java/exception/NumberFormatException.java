d1dbeb853f8bbba42442444978f4b1b21dfe211d
ADMIN <ADMIN@PLACEMENTLAB-97>
2020-02-22 18:14:13 +0530

    commit

---
+++ Pecunia-Banking-Management-System/src/main/java/exception/NumberFormatException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class NumberFormatException extends Exception{
+	public NumberFormatException(String Message) {
+		super(Message);
+	}
+	public NumberFormatException( ) {
+		super();
+	}
+}
